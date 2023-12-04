c700ba8202950871654709f846eda57955f426ce
Taras Protsyshyn <protsyshyn.t@gmail.com>
2023-12-04 19:39:54 +0200

    fix: delete unused file

--- src/utils/extractTasksList.ts
+++
@@ -1,7 +0,0 @@
-// export const extractTasksList = (content: string, list: string[]) => {
-//   let buffer = content;
-//   let currList = [];
-
-//   const startIn = buffer.indexOf('- [');
-
-// };
